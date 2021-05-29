8af9bae58ce591dfc38968802090deb5ab85c9d7
Bohnel <m.ludwikowski1@gmail.com>
2021-05-29 16:22:25 +0200

    zoom+sass

--- CirclePackingTeam🚀/script.js
+++ CirclePackingTeam🚀/script.js
@@ -1,30 +1,34 @@
 // Fenstergröße Layout definieren
-let vWidth = window.innerWidth;
-let vHeight = window.innerHeight;
-let margin = 150;
+const margin = 100
+const vWidth = window.innerWidth
+const vHeight = window.innerHeight - margin
+let clicked = false
 
 // Diagramm rendern
 let g = d3.select('svg')
     .attr('width', vWidth)
     .attr('height', vHeight)
-    .select('g');
+    .select('g')
 
 // Div für Label rendern
 var div = d3.select("body").append("div")
     .classed("tooltip", true)
-    .style("opacity", 0);
+    .style("opacity", 0)
 
 // Daten aus der JSON Datei laden und Zeichnen Funktion aufrufen
-d3.json('/data/rental.json').then((data) => {
-    console.log(data);
-    drawViz(data);
-});
+d3.json('/data/rental.copy.json').then((data) => {
+    //größten Kreis als Wurzel abspeichern
+    root = d3.pack(data)
+    // Diagramm zeichnen
+    drawViz(data)
+})
 
-// Zeichnen Funktion
+// Zeichen Funktion
 function drawViz(data){
     // Größten Kreis Layout definieren
     let vLayout = d3.pack()
-        .size([vWidth, vHeight - margin]);
+        .size([vWidth, vHeight - margin])
+        .padding(10)
 
     // Größten Kreis erzeugen indem alle untere Kindervalues aufsummiert werden
     let vRoot = d3.hierarchy(data)
@@ -39,32 +43,75 @@ function drawViz(data){
     let vNodes = vRoot.descendants();
     
     // Dem Kreis Layout den größten Kreis übergeben
-    vLayout(vRoot);
+    vLayout(vRoot)
 
     // Kinderkreise rendern
     let vSlices = g.selectAll('circle')
         .data(vNodes)
         .enter()
         .append('circle')
-        .on("mouseover", (d,i) => {
-            div.transition().duration(200).style("opacity", 1);
-            div.html(i.data.name + "</br>" + i.data.value);
-        })
-        .on("mousemove", () => { 
-            div.style("top", (event.pageY)+"px").style("left",(event.pageX)+"px");})
-        .on("mouseout", (d,i) => {
-            div.transition().duration(200).style("opacity", 0);
-        })
-        .call(d3.zoom().on("zoom", (event) => {
-            vSlices.attr('transform', event.transform);
-        }))
+        .attr("fill-opacity", ".3")
+        .attr("class", (d) => { return d.children ? "node" : "leaf"; })
+        // Tooltips bei Mausbewegung
+            .on("mouseover", handleMouseOver)
+            .on("mousemove", () => { div.style("top", (event.pageY) + -65 + "px").style("left", (event.pageX) + 10 + "px") })
+            .on("mouseout", handleMouseOut)
+            .on("click", handleClick)
 
     vSlices.append("text")
-        .attr("dy", ".3em")
-        .text((d) => { return d.data.name; })
+        .attr("dx", 12)
+        .attr("dy", ".35em")
+        .text((d,i) => { return i.name; })
 
     // Diagramm zeichnen
     vSlices.attr('cx', (d) => { return d.x; })
             .attr('cy', (d) => { return d.y; })
-            .attr('r', (d) => { return d.r; });
+            .attr('r', (d) => { return d.r; })
+
+    function handleMouseOver(d,i) {
+        d3.select(this)
+            .transition()
+            .duration(100)
+            .attr("stroke", "#EDF6F9")
+            .attr("stroke-opacity", 1)
+        div.transition().duration(200).style("opacity", .9)
+        div.html("name: " + i.data.name + "</br>" + "amount: " + i.data.value)
+    }
+
+    function handleMouseOut() {
+        d3.select(this)
+            .transition()
+            .duration(300)
+            .attr("stroke-opacity", 0)
+        div.transition().duration(200).style("opacity", 0)
+    }
+
+    function handleClick() {
+        let x, y, k
+        x = d3.select(this).attr("cx")
+        y = d3.select(this).attr("cy")
+        k = d3.select(this).attr("r")
+        k = 4 //calculation
+
+        if (clicked === true) {
+            // Zweiter Klick
+            clicked = false
+            g.transition()
+                .ease(d3.easeBack,9)
+                .duration(750)
+                .attr("transform", "translate(" + vWidth / 2 + "," + vHeight / 2 
+                    + ")scale()")
+        } else {
+            //Erster Klick
+            clicked = true
+            g.transition()
+                .ease(d3.easeExp,10)
+                .duration(750)
+                .attr("transform", "translate(" + vWidth / 2 + "," + vHeight / 2 
+                    + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+            vSlices.transition()
+                .duration(750)
+                // .style("stroke-width", 1.5 / k + "px");
+        }
+    } 
 }
