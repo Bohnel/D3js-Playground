01666e1537f402827f68cdf4f54235381fa29f41
Bohnel <m.ludwikowski1@gmail.com>
2021-05-31 17:50:33 +0200

    Tooltip, Zoom, Labels

--- CirclePackingTeam🚀/script.js
+++ CirclePackingTeam🚀/script.js
@@ -1,7 +1,7 @@
 // Fenstergröße Layout definieren
 const margin = 100
-const vWidth = window.innerWidth
-const vHeight = window.innerHeight - margin
+const vWidth = 800
+const vHeight = 800
 let clicked = false
 
 // Diagramm rendern
@@ -11,8 +11,9 @@ let g = d3.select('svg')
     .select('g')
 
 // Div für Label rendern
-var div = d3.select("body").append("div")
-    .classed("tooltip", true)
+let tooltip = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip")
     .style("opacity", 0)
 
 // Daten aus der JSON Datei laden und Zeichnen Funktion aufrufen
@@ -27,8 +28,8 @@ d3.json('/data/rental.copy.json').then((data) => {
 function drawViz(data){
     // Größten Kreis Layout definieren
     let vLayout = d3.pack()
-        .size([vWidth, vHeight - margin])
-        .padding(10)
+        .size([vWidth, vHeight])
+        .padding(5)
 
     // Größten Kreis erzeugen indem alle untere Kindervalues aufsummiert werden
     let vRoot = d3.hierarchy(data)
@@ -49,49 +50,101 @@ function drawViz(data){
     let vSlices = g.selectAll('circle')
         .data(vNodes)
         .enter()
+        .append("g") //svg group for label
         .append('circle')
         .attr("fill-opacity", ".3")
-        .attr("class", (d) => { return d.children ? "node" : "leaf"; })
+        .attr("id", (d) => { if (d.data.main === "main") { return "main" }})
+        // .attr("class", (d) => { return d.children ? "node" : "leaf"; })
         // Tooltips bei Mausbewegung
             .on("mouseover", handleMouseOver)
-            .on("mousemove", () => { div.style("top", (event.pageY) + -65 + "px").style("left", (event.pageX) + 10 + "px") })
+            .on("mousemove", () => { tooltip.style("top", (event.pageY) + -65 + "px").style("left", (event.pageX) + 10 + "px") })
             .on("mouseout", handleMouseOut)
             .on("click", handleClick)
 
-    vSlices.append("text")
-        .attr("dx", 12)
-        .attr("dy", ".35em")
-        .text((d,i) => { return i.name; })
+    d3.select(window).on("load", showLabelsInit())
+
+    //Show labels on load
+    function showLabelsInit(d) {
+        g.selectAll("g")
+            .filter((d) => { return d.children })
+            .append("text")
+            .attr("class", "continent label")
+            .text((d) => { return d.data.name })
+            .attr("x", (d) => { return d.x })
+            .attr("y", (d) => { return d.y + 5 })
+            .attr("text-anchor", "middle")
+            .attr("font-size", ".8em")
+    }
+
+    //Show labels on click
+    function showLabels(d) {
+        console.log(clicked)
+        g.selectAll("g")
+            .filter((d) => { if (clicked === false) { 
+                    return !d.children } 
+                else { 
+                    return d.children }})
+            .append("text")
+            .attr("x", (d) => { return d.x })
+            .attr("y", (d) => { return d.y + 5 })
+            .attr("class", (d) => { if ( d.children ) { return "continent" } else { return "country label" }})
+            .text((d) => { return d.data.name })
+            .attr("font-size", 0)
+            .attr("text-anchor", "middle")
+            .transition()
+            .duration(150)
+            .attr("font-size", ".8em")
+    }
 
     // Diagramm zeichnen
     vSlices.attr('cx', (d) => { return d.x; })
             .attr('cy', (d) => { return d.y; })
             .attr('r', (d) => { return d.r; })
 
+    //Show tooltip and add circle stroke
     function handleMouseOver(d,i) {
+        if (clicked === false) {
         d3.select(this)
+            .filter((d) => { return d.children })
             .transition()
             .duration(100)
             .attr("stroke", "#EDF6F9")
             .attr("stroke-opacity", 1)
-        div.transition().duration(200).style("opacity", .9)
-        div.html("name: " + i.data.name + "</br>" + "amount: " + i.data.value)
+            .attr("fill-opacity", .4)
+            
+        } else {
+            d3.select(this)
+            .filter((d) => { return !d.children })
+            .transition()
+            .duration(100)
+            .attr("stroke", "#EDF6F9")
+            .attr("stroke-opacity", 1)
+            .attr("fill-opacity", .4)
+            tooltip.transition().duration(200).style("opacity", .9)
+            tooltip.html("name: " + i.data.name + "</br>" + "amount: " + i.data.value)
+        }
     }
 
+    //remove tooltip and circle stroke
     function handleMouseOut() {
         d3.select(this)
             .transition()
             .duration(300)
             .attr("stroke-opacity", 0)
-        div.transition().duration(200).style("opacity", 0)
+            .attr("fill-opacity", .3)
+            tooltip.transition().duration(200).style("opacity", 0)
     }
 
+    //zoom on click
     function handleClick() {
+        showLabels()
         let x, y, k
         x = d3.select(this).attr("cx")
         y = d3.select(this).attr("cy")
-        k = d3.select(this).attr("r")
-        k = 4 //calculation
+        r = d3.select(this).attr("r")
+        k = 4
+        console.log(k);
+        
 
         if (clicked === true) {
             // Zweiter Klick
@@ -101,6 +154,7 @@ function drawViz(data){
                 .duration(750)
                 .attr("transform", "translate(" + vWidth / 2 + "," + vHeight / 2 
                     + ")scale()")
+            g.selectAll(".country").remove()
         } else {
             //Erster Klick
             clicked = true
@@ -112,6 +166,7 @@ function drawViz(data){
             vSlices.transition()
                 .duration(750)
                 // .style("stroke-width", 1.5 / k + "px");
+            g.selectAll(".continent").remove()
         }
     } 
 }
